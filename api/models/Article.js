a09de00daa4a475dbef4e9fcb4a5ec25586cb062
Alex Shestmintsev <shestmintsev@gmail.com>
2015-05-14 21:44:55 +0300

    init project

---
+++ api/models/Article.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+  attributes: {
+
+    title: {
+      type: 'string',
+      maxLength: 120,
+      required: true
+    },
+    description: {
+      type: 'string',
+      required: true
+    },
+    content: {
+      type: 'string',
+      required: true
+    }
+
+  }
+};
+
