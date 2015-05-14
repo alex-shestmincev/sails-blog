99188d1c1673118858b7f31d7354a6ea32eea306
Alex Shestmintsev <shestmintsev@gmail.com>
2015-05-14 22:34:42 +0300

    REST for article

--- api/controllers/ArticleController.js
+++ api/controllers/ArticleController.js
@@ -1,11 +1,82 @@
-/**
- * ArticleController
- *
- * @description :: Server-side logic for managing articles
- * @help        :: See http://links.sailsjs.org/docs/controllers
- */
-
 module.exports = {
-	
+  create: function (req, res){
+    var params = {
+      description: req.param('description'),
+      content: req.param('content'),
+      title: req.param('title'),
+    }
+
+    Article.create(params).exec(function(err, article){
+      res.redirect('/article/watch/' + article.id);
+      if (err) return res.send(500);
+    });
+  },
+
+  update: function(req, res){
+    var Id = req.param('id');
+
+    var elem = {
+      description : req.param('description'),
+      content: req.param('content'),
+      title: req.param('title'),
+    }
+
+    Article.update(Id, elem).exec(function(err){
+      if (err) return res.send(500);
+      res.redirect('/article');
+    });
+  },
+
+  delete:  function (req, res){
+    var Id = req.param('id');
+
+    Article.destroy(Id).exec(function(err){
+      if (err) return res.send(500);
+      res.redirect('/article');
+    });
+  },
+
+  index: function (req, res){
+    Article
+      .find()
+      .sort('id DESC')
+      .limit(5)
+      .exec(function(err, articles){
+        if (err) return res.send(500);
+        res.view({
+          articles: articles
+        });
+      });
+
+  },
+
+  watch: function (req, res){
+    var Id = req.param('id');
+    Article.findOne(Id).exec(function(err, article){
+      if (!article) return res.send(404);
+      if (err) return res.send(500);
+      res.view({
+        article: article
+      });
+    });
+  },
+
+  page: function (req, res){
+    var page = req.param('page');
+
+    Article
+      .find()
+      .sort('id DESC')
+      .paginate({
+        page: page,
+        limit: 5
+      })
+      .exec(function(err, articles){
+        if (err) return res.send(500);
+        res.view({
+          articles: articles
+        });
+      });
+  }
 };
 
